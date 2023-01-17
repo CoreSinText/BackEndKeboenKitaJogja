8e62bdd8340aede0b96724b982bc0d7c286648af
CoreSinText <coresintext@gmail.com>
2023-01-18 02:30:44 +0700

    update

--- deleteData.ts
+++ deleteData.ts
@@ -1,13 +1,13 @@
 import { client } from "./connection.ts";
 
-export function deleteProduk(idProduk:number){
-client.query(`DELETE FROM produk WHERE produk.produk_id = ${idProduk}`)
+export async function deleteProduk(idProduk: number) {
+    return await client.query(`DELETE FROM produk WHERE produk.produk_id = ${idProduk}`)
 }
 
-export function deleteTransaksi(idTransaksi:number){
-    client.query(`DELETE FROM transaksi_penjualan WHERE transaksi_penjualan.transaksi_id = ${idTransaksi}`)
+export async function deleteTransaksi(idTransaksi: number) {
+    return await client.query(`DELETE FROM transaksi_penjualan WHERE transaksi_penjualan.transaksi_id = ${idTransaksi}`)
 }
 
-export function deleteAdmin(idAdmin:number){
-    client.query(`DELETE FROM user WHERE user.user_id = ${idAdmin}`)
+export async function deleteAdmin(idAdmin: number) {
+    return await client.query(`DELETE FROM user WHERE user.user_id = ${idAdmin}`)
 }
